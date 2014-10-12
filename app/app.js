4e24aa0674336cc580ce8a8bf1a38c9af3ff8139
Nadir Izrael <nadir.izr@gmail.com>
2014-10-12 13:05:49 +0300

    working on it

--- app/app.js
+++ app/app.js
@@ -1,3 +1,17 @@
-"use strict";
+var app = angular.module("RTC", []);
 
-var RTC = angular.module('RTC', []);
+current_user = null;
+
+app.controller("MainCtrl", function($scope){
+  // Expose $scope for debugging.
+  g1 = $scope;
+
+  $scope.password = "123";
+  $scope.current_user = null;
+
+  $scope.submit = function() {
+    if ($scope.username == "Sella") {
+      $scope.current_user = {name: "Sella"};
+    }
+  };
+});
