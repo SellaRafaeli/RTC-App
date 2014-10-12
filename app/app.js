97ba1be51e286e8e3aef5dbad293ae6be154d372
Sella Rafaeli <sella.rafaeli@gmail.com>
2014-10-12 15:07:10 +0300

    signin and routing with couple pages

--- app/app.js
+++ app/app.js
@@ -1,17 +1,29 @@
-var app = angular.module("RTC", []);
+var app = angular.module("RTC", ['ngRoute']);
 
-current_user = null;
+app.config(function($routeProvider){
+  $routeProvider.when("/",
+    {
+      templateUrl: "login.html",
+      controller: "loginCtrl"
+    }
+  	).when('/profile', {
+  		templateUrl: "profile.html",      
+  		//controller: "MainCtrl"
+    }).when('/profile2', {
+  		templateUrl: "profile2.html",      
+  		//controller: "MainCtrl"
+    });
+});
 
-app.controller("MainCtrl", function($scope){
-  // Expose $scope for debugging.
-  g1 = $scope;
 
-  $scope.password = "123";
-  $scope.current_user = null;
+
+app.controller("loginCtrl", function($scope, $routeParams, $location, $rootScope){  
+  g1 = $scope; // Expose $scope for debugging.  
 
   $scope.submit = function() {
-    if ($scope.username == "Sella") {
-      $scope.current_user = {name: "Sella"};
+    if (true || ($scope.username == "Sella")) {    	
+      $rootScope.current_user = {username: $scope.username};      
+      $location.path('/profile')
     }
   };
 });
